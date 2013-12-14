0cb86ff03e926b493b9d88b57d77d1a253dec4e2
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2013-12-14 17:25:48 +0200

    Session five attempt - with Adi Bona

--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -1,2 +1,12 @@
 describe("Game of life", function() {
+    describe("Cell", function () {
+        it('dies if it has fewer than three neighbors', function (){
+            var cell = new Cell(true);
+            cell.addNeighbors(
+                new Cell(true)
+            );
+            cell.update();
+            expect(cell.isAlive).toEqual(false);
+        });
+    });
 });
--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,2 +1,43 @@
-function GameOfLife() {
-}
+// no conditionals
+//  no if/else, while, foreach, switch
+// 4 lines per method max
+// 3d universe
+
+// what works: validation
+// almost nothing else
+
+(function GameOfLife() {
+
+    var _kill = function() {
+        this.isAlive = false;
+    };
+
+    var stateHandler = Array.apply(null, new Array(26)).map(function() {
+        return function () {};
+    });
+    stateHandler[0] = _kill;
+    stateHandler[1] = _kill;
+
+    var Universe = function () {
+    };
+
+    var Cell = function (isAlive) {
+        this.isAlive = isAlive;
+        this.neighbors = [];
+    }
+    Cell.prototype.addNeighbors = function () {
+        this.neighbors = this.neighbors.concat.apply(this.neighbors, arguments);
+    };
+    Cell.prototype._getLivingNeighbors = function () {
+        return this.neighbors.filter(function(cell){
+            return cell.isAlive;
+        }).length;
+    }
+    Cell.prototype.update = function () {
+        stateHandler[this._getLivingNeighbors()].call(this);
+    }
+
+    window.Universe = new Universe();
+    window.Cell = Cell;
+
+}());
