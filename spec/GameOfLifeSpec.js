667818ced69fe1f4e81289b2d11ef62d17e9b633
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2013-12-14 12:36:45 +0200

    Session with Vasile Jureschi

--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -1,2 +1,33 @@
 describe("Game of life", function() {
+    describe("Cell", function () {
+        var cell;
+        it("it throws up on invalid state", function () {
+            expect(function() { new Cell(); }).toThrow();
+        });
+        it("can be created alive", function () {
+            expect(function() { new Cell(true); }).not.toThrow();
+        });
+        it("can be created dead", function () {
+            expect(function() { new Cell(false); }).not.toThrow();
+        });
+        it("always has a valid state", function () {
+            cell = new Cell(true);
+            expect(cell.state).not.toBeUndefined();
+        });
+    });
+    describe("World", function () {
+        it("initializes with an empty list of cells", function () {
+            var world = new World();
+            expect(world.cells.length).toEqual(0);
+        });
+        describe("creation", function () {
+            var world = new World();
+            it("throws when creating with incorrect number of cells", function () {
+                expect(world.create('test')).toThrow();
+            });
+            it("doesn't throw when creating with incorrect number of cells", function () {
+                expect(world.create(1)).not.toThrow();
+            });
+        });
+    });
 });
--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,2 +1,28 @@
 function GameOfLife() {
 }
+
+// seed cells
+// create cells from seed
+// update cells step
+// cells have simple alive or dead state
+// cells know their neighbours
+// cells receive their neighbours randomly
+
+function World(){
+    this.cells = [];
+}
+World.prototype.create = function (numberOfLivingCells) {
+    if (typeof numberOfLivingCells !== 'number') {
+        throw "Need a valid number of cells";
+    }
+    for (var i = 0, l = numberOfLivingCells; i < l; i ++) {
+        this.cells.push( new Cell(true) );
+    }
+}
+
+function Cell(state) {
+    if (state !== true && state !== false) {
+        throw "Need valid state.";
+    }
+    this.state = state || true;
+}
