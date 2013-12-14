6aab666356f64f91e6097236f6a644ca0e239ed4
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2013-12-14 11:20:40 +0200

    Session with Tudor Potecaru.

--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -1,2 +1,41 @@
 describe("Game of life", function() {
+  describe("Cell", function () {
+    it('knows about its neighbours', function() {
+      var cell = new Cell(true, []);
+      expect(cell.neighbours).not.toBeUndefined();
+    });
+    
+    describe('updates its state according to its neighbours', function(){
+      it("dies when it has fewer than two living neighbours and is alive", function(){
+        var cell = new Cell(true, [
+          new Cell(false,[]),
+          new Cell(false,[])
+        ]);
+        cell.update();
+        expect(cell.isAlive).toEqual(false);
+        
+        var cell = new Cell(false, [
+          new Cell(false,[]),
+          new Cell(false,[])
+        ]);
+        cell.update();
+        expect(cell.isAlive).not.toEqual(true);
+      });
+      it("lives when it has two or three living neighbours and is alive", function(){
+        var cell = new Cell(true, [
+          new Cell(true,[]),
+          new Cell(true,[])
+        ]);
+        cell.update();
+        expect(cell.isAlive).toEqual(true);
+        
+        var cell = new Cell(false, [
+          new Cell(true,[]),
+          new Cell(true,[])
+        ]);
+        cell.update();
+        expect(cell.isAlive).not.toEqual(true);
+      });
+    });
+  });
 });
--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,2 +1,45 @@
 function GameOfLife() {
 }
+
+// cell object
+// has current and next state
+// knows its neighbours
+// it updates itself
+
+// representation
+// initial seed
+// has a generation step
+
+function Cell(initialState, neighbours){
+  this.isAlive = initialState;
+  this.neighbours = neighbours;
+}
+
+Cell.prototype._extractNumberOfLivingNeighbours = function (){
+    var i,l,v, livingCells = 0;
+
+    for (i = 0, l = this.neighbours.length; i < l; i ++) {
+        v = this.neighbours[i];
+        if (v.isAlive) {
+            livingCells++;
+        }
+    }
+
+    return livingCells;
+}
+
+Cell.prototype.update = function () {
+    var living = this._extractNumberOfLivingNeighbours();
+
+    if(this.isAlive) {
+      if (living < 2) {
+          this.isAlive = false;
+          return;
+      }
+      
+      if(living === 2 || living === 3) {
+          this.isAlive = true;
+          return;      
+      }
+    }    
+}
